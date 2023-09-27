5ad9642b638e7ac9c1a57cd165af441335db6e82
andrew.sales <andjsales@gmail.com>
2023-09-27 16:52:49 -0500

    connected files together to create story

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, render_template
+from stories import story
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/homepage')
+@app.route('/')
 def homepage():
     """Return to homepage"""
-    return render_template("homepage.html")
+
+    prompts = story.prompts
+    return render_template("homepage.html", prompts=prompts)
 
 
 @app.route('/story')
-def story():
+def story_result():
     """Shows the resulting story for those answers"""
-    return render_template("story.html")
+
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
 
 
 if __name__ == '__main__':
